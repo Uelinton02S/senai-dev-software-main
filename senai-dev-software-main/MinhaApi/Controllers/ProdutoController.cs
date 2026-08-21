9666e1b75226438aab0244abaa4828a6e1ae34c8
Uelinton <Uelintonsantiago13@gmail.com>
2026-08-21 20:38:03 -0300

    Atualiza Controller

--- senai-dev-software-main/MinhaApi/Controllers/ProdutoController.cs
+++ senai-dev-software-main/MinhaApi/Controllers/ProdutoController.cs
@@ -48,5 +48,28 @@ public class ProdutoController : ControllerBase
             "estoque":,10
         }
     }
+    // PUT /api/produto/1
+    [HttpPut("{id}")]
+    public IActionResult Update(
+        int id,
+        [FromBody] Produto protudo)
+    {
+        var atualizado = _service.Update(id, produto);
+        if(atualizado == null)
+        return NotFound();
+        
+        return Ok(atualizado); 
+    }
+    
+// DELETE /api/produto/1
+[HttpDelete("{id}")]
+public IActionResult Delete(int id)
+{
+    var deletado = _service.Delete(id);
 
+    if (!deletado)
+        return NotFound();
+
+    return NoContent();
+}
 }
