609d7eb3979ed7b46ca2d2ce7df7a5ff69290e7f
Uelinton02S <uelintonsantiago13@gmail.com>
2026-09-01 12:00:44 -0300

    01/09/2026

---
+++ senai-dev-software-main/MinhaApi/Program.cs
@@ -0,0 +1,23 @@
+using MinhaApi.Repositories;
+using MinhaApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+
+builder.Services.AddScoped<IProdutoService, ProdutoService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+app.Run();
