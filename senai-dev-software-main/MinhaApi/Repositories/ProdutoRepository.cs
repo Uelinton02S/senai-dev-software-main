813b951134b884e1e7d2fc20b333b7aa74530754
Uelinton <Uelintonsantiago13@gmail.com>
2026-09-02 20:46:21 -0300

    Adiciona banco de dados

--- senai-dev-software-main/MinhaApi/Repositories/ProdutoRepository.cs
+++ senai-dev-software-main/MinhaApi/Repositories/ProdutoRepository.cs
@@ -34,11 +34,24 @@ public ProdutoRepository(IConfiguration config)
   }
     public Produto? GetById(int id) => _db.FirstOrDefault(p => p.Id == id);
 
-    public void Add(Produto p)
-    {
-        p.Id = _db.Any() ? _db.Max(x => x.Id) + 1 : 1;
-        _db.Add(p);
-    }
+   public void Add(Produto p) {
+    using var conn = new MySqlConnection(_connectionString);
+    conn.Open();
+
+    string sql = @"INSERT INTO produtos (nome, preco, estoque, ativo) 
+                   VALUES (@Nome, @Preco, @Estoque, @Ativo);
+                   SELECT LAST_INSERT_ID();";
+
+    using var cmd = new MySqlCommand(sql, conn);
+    cmd.Parameters.AddWithValue("@Nome", p.Nome);
+    cmd.Parameters.AddWithValue("@Preco", p.Preco);
+    cmd.Parameters.AddWithValue("@Estoque", p.Estoque);
+    cmd.Parameters.AddWithValue("@Ativo", p.Ativo);
+
+    // Executa a inserção e recupera o ID gerado pelo MySQL
+    var idGerado = cmd.ExecuteScalar();
+    p.Id = Convert.ToInt32(idGerado);
+}
 
     public void Update(Produto p)
     {
