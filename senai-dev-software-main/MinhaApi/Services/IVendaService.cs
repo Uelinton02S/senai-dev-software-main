db0ba2992ff444f5e1c391604df7e6e80dc23d3d
UelintonSSJ <uelintonsantiago13@gmail.com>
2026-09-16 14:22:09 -0300

    Refactor IVendaService interface methods

--- senai-dev-software-main/MinhaApi/Services/IVendaService.cs
+++ senai-dev-software-main/MinhaApi/Services/IVendaService.cs
@@ -2,11 +2,11 @@ using MinhaApi.Models;
 
 namespace MinhaApi.Services;
 
-
 public interface IVendaService
 {
-    Venda Add(int id,Venda venda);
-    Venda? Update(int idclientes,int idprodutos,Venda venda);
-    Venda?GetById(int id,Venda venda);
-    Venda Create (int id, Venda venda);
+    Venda Create(Venda venda);
+
+    Venda? Update(int id, Venda venda);
+
+    Venda? GetById(int id);
 }
